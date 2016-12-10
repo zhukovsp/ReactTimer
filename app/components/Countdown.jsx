0aa53b89cc5de14e225871027f554ae57c7c7ab7
Sergey <sergavo@gmail.com>
2016-12-10 14:06:28 +0300

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+// var Countdown = (props) => {
+//   return (
+//     <div>
+//       <h1 className="text-center page-title">Countdown.jsx</h1>
+//     </div>
+//   )
+// };
+
+var Countdown = React.createClass({
+  render: function () {
+    return <p>Countdown.jsx</p>;
+  }
+});
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+
+// var Timer = (props) => {
+//   return (
+//     <div>
+//       <h1 className="text-center page-title">timer.jsx</h1>
+//     </div>
+//   )
+// };
+
+var Timer = React.createClass({
+  render: function () {
+    return <p>Timer.jsx</p>;
+  }
+});
+
+
+module.exports = Timer;
